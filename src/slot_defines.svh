bceaa6825539372cfe0c269c6da9af8ddafebb8e
Evansika <xinting@umich.edu>
2025-12-10 13:10:02 -0500

    update padring

--- src/slot_defines.svh
+++ src/slot_defines.svh
@@ -7,8 +7,8 @@
 // Signal pads
 `define NUM_INPUT_PADS 10
 `define NUM_BIDIR_PADS 13
-`define NUM_ANALOG_PADS 6
-`define NUM_UNUSED_PADS 19
+`define NUM_ANALOG_PADS 7
+`define NUM_UNUSED_PADS 18
 
 `endif
 
