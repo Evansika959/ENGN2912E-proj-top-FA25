7142613e99dc1ed086ee84f0091e549e49532718
Evansika <xinting@umich.edu>
2025-12-17 21:15:12 -0500

    update pad ring

--- scripts/padring.py
+++ scripts/padring.py
@@ -56,7 +56,7 @@ class PadringFlow(SequentialFlow):
         Odb.CheckMacroAntennaProperties,
         Odb.ManualMacroPlacement,
         KLayout.StreamOut,
-        KLayout.DRC,
+        # KLayout.DRC,
         KLayout.SealRing,
     ]
 
